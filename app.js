46977280543e0008fab0ef4336e3d756753633f7
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-15 06:09:39 +0200

    Install packages and setup app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// cors
+app.use(cors());
+
+// Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
