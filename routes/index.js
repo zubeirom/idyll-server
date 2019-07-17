671d4766feca2a9d93175db5ec94fdd4844283a5
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-17 10:57:45 +0200

    google route for google requests

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,22 @@
 /* eslint-disable no-underscore-dangle */
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
-const JSONAPISerializer = require('jsonapi-serializer').Serializer;
-const JSONAPIError = require('jsonapi-serializer').Error;
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('a6621af2728b4b689ba4f4c17255b292');
+const googleTrends = require('google-trends-api');
+const https = require('https');
 
 const router = express.Router();
 
-router.get('/news', asyncHandler(async (req, res, next) => {
+router.get('/google', asyncHandler(async (req, res, next) => {
     try {
-        newsapi.v2.topHeadlines({
-            language: 'en',
-        }).then(response => {
-            console.log(response);
-            /*
-              {
-                status: "ok",
-                articles: [...]
-              }
-            */
-        });
+        googleTrends.dailyTrends({ geo: req.query.country })
+            .then(function (results) {
+                const finalRes = JSON.parse(results)
+                res.status(200).json(finalRes);
+                next();
+            })
+            .catch(function (err) {
+                next(err)
+            });
     } catch (error) {
         next(error);
     }
